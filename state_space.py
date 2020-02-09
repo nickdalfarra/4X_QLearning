6486a564bd0ac8bc4edcbcee6ade57d4ada5286d
nickdalfarra <dalfarra.nick@queensu.ca>
2020-02-08 20:43:20 -0500

    Created Dif1 and Dif2 metric child classes and the Metric parent class. Beginning work on creating model fitting.

--- state_space.py
+++ state_space.py
@@ -1,6 +1,8 @@
 """State space for Q-Learning agents."""
 from functools import reduce
-import metrics as met
+from metrics import Dif1, Dif2
+from math import floor, ceil
+import pandas as pd
 
 class StateSpace():
     """States of the RL agent.
@@ -9,29 +11,30 @@ class StateSpace():
     def __init__(self, metrics: list):
 
         if type(metrics) != "list": TypeError("metrics must be a list of Metric objects.")
-        if not metrics: raise ValueError("StateSpace metric list must be non-empty.")
+        if not metrics: raise ValueError("metric list must be non-empty.")
 
         self.metrics = metrics
 
+        # get properties of the state space
         if len(metrics) > 1:
             self.cardinality = reduce(lambda a,b : a.num_vals * b.num_vals, self.metrics)
-            self.max_memory = reduce(lambda a,b: max(a.memory, b.memory), self.metrics)
         elif len(metrics) == 1:
             self.cardinality = metrics[0].num_vals
-            self.max_memory = metrics[0].memory
             
-    def __str__(self): return "state space of size " + str(self.cardinality)
+    def __str__(self): return "State space of size " + str(self.cardinality)
 
 def main():
-    dif1 = met.Metric("dif1", 1, 4)
-    dif2 = met.Metric("dif2", 3, 3)
-    metrics = [dif1, dif2]
+    # prep data
+    prices = pd.read_csv('USD_CADHistoricalData.csv', header = 0, usecols = ['Price'])
+    training_split = 0.8
+    training_data = prices.head(ceil(len(prices)*training_split))
+    # testing_data = prices.tail(floor(len(prices)*(1-training_split)))
 
-    X = StateSpace(metrics)
+    dif1 = Dif1(1, 10, training_data)
+    dif2 = Dif2(0, 10, training_data)
+
+    X = StateSpace([dif1, dif2])
     print(X)
-
-def type_test():
-    X = StateSpace(['a'])
 
 if __name__ == "__main__":
     main()
